b9ca1bdbcd68adef812b4b217c4231d85b8774a1
Artem Vitkovsky <workwith9512@gmail.com>
2023-06-15 08:44:41 +0300

    fix: исправление поля игры

--- game.py
+++ game.py
@@ -6,20 +6,20 @@ board = list(range(1, 10))
 def draw_board(board):
     print('-' * 13)
     for i in range(3):
-        print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[1 + i * 3], '|')
+        print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
         print('-' * 13)
 
 
 def take_input(player_token):
     valid = False
     while not valid:
-        player_answer = input(f'{player_token} Ваш ход')
+        player_answer = input(f'{player_token} Ваш ход: ')
         try:
             player_answer = int(player_answer)
         except:
             print('Некоррентный ввод')
             continue
-        if player_answer in range(1, 10):
+        if player_answer >= 1 and player_answer <= 9:
             if(str(board[player_answer - 1]) not in "XO"):
                 board[player_answer - 1] = player_token
                 valid = True
